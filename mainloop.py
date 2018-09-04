fb2e0dcadd0f4dc16758675ee06b43f9400924a1
chernand1 <c__hernandez@hotmail.com>
2018-09-03 23:10:10 -0400

    Git Hub test 2 commit from pycharm

--- mainloop.py
+++ mainloop.py
@@ -8,32 +8,17 @@ from fetchScores import football
 
 import time
 
-# matchToWatch = football('football', 'cfl')
 matchToWatch = football(league='nfl')
-#print(matchToWatch.league())
-#matchToWatch.league = 'cfl'
 
 print(matchToWatch.league)
 matchToWatch.readHtmlFile()
 
 print(matchToWatch.printUrl())
-#matchToWatch.listMatchups()
-#print(newyorkgiants.FullDescription['url'])
-#newyorkgiants.readHtmlFile()
-#newyorkgiants.printMatch(1)
 matchToWatch.printAllMatches()
 matchToWatch.listTeams()
 
-'''canadiens = hockey()
-canadiens.readHtmlFile()
-canadiens.listMatchups()
-canadiens.printAllMatches()
-'''
 
 while(1):
-    #print("Updated value = ", matchToWatch.readHtmlFile("NY Giants"))
-    #matchToWatch.getMatchDetail()
-    #print(matchToWatch.getMatchDetail(13))
     print(matchToWatch.getMatchNumber("NY Giants"))
     print(matchToWatch.getMatchDetail(matchToWatch.getMatchNumber("NY Giants")))
     time.sleep(5)
