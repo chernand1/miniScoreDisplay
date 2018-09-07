9a3f1ae84b34d4ff44b1f2af5c73ddaa4c02c3a8
chernand1 <c__hernandez@hotmail.com>
2018-09-06 23:42:31 -0400

    Git Hub test 2 commit from pycharm

--- miniDisplayScore.py
+++ miniDisplayScore.py
@@ -2,9 +2,9 @@ from luma.core import cmdline, error
 from PIL import Image, ImageDraw, ImageFont
 from luma.core.render import canvas
 from luma.core.virtual import viewport
+import smbus2
 
 class displayScore:
-
 
     def __init__(self):
         self.resolutionX = 96
@@ -14,7 +14,7 @@ class displayScore:
         self.bus = 'i2c'
         self.portNumber = '0'
 
-    def createDevice(self, type='luma', chip='ssd1331', width='96', height='64',bus='spi', portNo='0'):
+    def createDevice(self, type='luma', chip='ssd1331', width='96', height='64',bus='spi', portNo='0', bgcolor="black"):
 
         self.resolutionX = width
         self.resolutionY = height
@@ -29,32 +29,59 @@ class displayScore:
                 parser = cmdline.create_parser(description='luma.examples arguments')
                 args1 = parser.parse_args(args)
                 self.device = cmdline.create_device(args1)
+                #self.buffer = Image.new("RGBA", self.device.size, bgcolor)
+                #self.device.display(self.buffer.convert(self.device.mode))
+                self.virtual = viewport(self.device, width=self.device.width, height=self.device.height)
                 return 0
             except error.Error as e:
                 return e
+
+    def lcddisplaytest(self, segment_numbers):
+
+        I2C_COMM1 = 0x40
+        I2C_COMM2 = 0xC0
+        I2C_COMM3 = 0x80
+
+        i2cbus = smbus2.SMBus(1)
+        #a = i2cbus.i2c_rdwr(I2C_COMM1)
+        i2cbus.write_byte(I2C_COMM1,0)
+        #i2cbus.i2c_rdwr(I2C_COMM2)
+        #i2cbus.i2c_rdwr(5)
+        #i2cbus.i2c_rdwr(I2C_COMM3 + 0xf)
 
     def clearscreen(self, color="white"):
 
         background = Image.new("RGBA", self.device.size, color)
         self.device.display(background.convert(self.device.mode))
+
         return 0
 
     def fonttype(self):
         return {'0': "code2000", '1': "FreePixel", '2': "pixelmix"}
 
-    def print_characters(self, text, font, fontsize):
+    def print_characters(self, text, font, fontsize, posx=0, posy=0):
 
         fontsdir = "fonts/"
         makefont = ImageFont.truetype(fontsdir + font + ".ttf", fontsize)
-        # draw = ImageDraw.Draw(background)
-        with canvas(self.device) as draw:
-            w, h = draw.textsize(text, makefont)
-            #draw.multiline_text(0, "Please do not adjust your set", fill="black", align="center", spacing=-1)
 
-        # First measure the text size
-        with canvas(self.device) as draw:
-            w, h = draw.textsize(text, makefont)
+        # virtual = viewport(self.device, width=self.device.width, height=self.device.height)
 
-        virtual = viewport(self.device, width=max(self.device.width, w), height=max(h, self.device.height))
-        with canvas(virtual) as draw:
-            draw.text((0, 0), text, font=makefont, fill="white")
+        with canvas(self.virtual) as draw:
+            draw.text((0, posy), text, font=makefont, fill="blue")
+            draw.text((30, posy+10), text, font=makefont, fill="red")
+            draw.text((60, posy+20), text, font=makefont, fill="green")
+
+'''
+    def display(self, image):
+        self._last_image = image.copy()
+        self.device.display(image)
+
+
+    def savepoint(self):
+        """
+        Copies the last displayed image.
+        """
+        if self._last_image:
+            self.savepoints.append(self._last_image)
+            self._last_image = None
+'''
--- testDisplay.py
+++ testDisplay.py
@@ -1,14 +1,32 @@
 from miniDisplayScore import displayScore
+from tm1637 import TM1637
 import time
 
 display1 = displayScore()
-display1.createDevice(portNo='1')
+display1.createDevice(portNo='1', bgcolor="red")
+#display1.clearscreen(color="red")
+time.sleep(2)
 
-    # display1.clearscreen(color="red")
 availablefonts = display1.fonttype()
 print (availablefonts['1'])
+lcddisplay = TM1637(3,2)
+t = time.localtime()
 
-display1.print_characters("This is a test", availablefonts['1'], 15)
+d0 = lcddisplay.digit_to_segment[t.tm_hour // 10] if t.tm_hour // 10 else 0
+d1 = lcddisplay.digit_to_segment[t.tm_hour % 10]
+d2 = lcddisplay.digit_to_segment[t.tm_min // 10]
+d3 = lcddisplay.digit_to_segment[t.tm_min % 10]
+
+lcddisplay.set_segments([d0, d1, d2, d3])
+
+for i in range(1, 10):
+    #display1.print_characters(str(i) + ":" + "0", availablefonts['0'], 20, posy=i)
+    #lcddisplay.brightness = i + 6
+    #lcddisplay.set_segments([i, ((0x80 & (i << 7)) & 0x80) + d1, d2, d3])
+    lcddisplay.set_segments([0xff, 0xff, 0xff, 0xff])
+    #print(i<<7)
+    time.sleep(0.1)
+
 
 while(1):
     time.sleep(1)
